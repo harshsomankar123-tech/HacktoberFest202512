1882999a8f4e9672956eabcb6194e7a7585ffec4
harshsomankar123-tech <harshsomankar123@gmail.com>
2025-10-17 02:29:20 +0530

    Implement running sum function in Solution class

---
+++ 23.py
@@ -0,0 +1,8 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        ans =[]
+        c = 0
+        for x in nums:
+            c +=x
+            ans.append(c)
+        return ans
